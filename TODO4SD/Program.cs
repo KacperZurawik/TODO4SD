b75dd29903d3f8c1d183313d77aa72f3bbb38dc7
KacperZurawik <kaacper181@gmail.com>
2024-06-12 14:14:30 +0200

    Code has been cleared

--- TODO4SD/Program.cs
+++ TODO4SD/Program.cs
@@ -10,44 +10,6 @@ namespace TODO4SD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TODO4SD 1.0");
-            Console.WriteLine("Fast TODO list for Service Desk");
-            Console.WriteLine("Current date: " + DateTime.Now);
-            Console.WriteLine();
-
-
-
-            //menu switchase
-
-            Console.WriteLine("1. Create new task");
-            Console.WriteLine("2. Show in progress tasks");
-            Console.WriteLine("3. Show tasks that have been done");
-
-            string userChoice = Console.ReadLine();
-            switch (userChoice)
-            {
-                case "1":
-                    Console.Write("Write down your first task here: ");
-                    var userTodo = Console.ReadLine();
-                    Console.WriteLine(userTodo);
-                    break;
-                case "2":
-                    Console.WriteLine("b");
-                    break;
-                case "3":
-                    Console.WriteLine("c");
-                    break;
-                default:
-                    break;
-            }
-
-            Console.ReadLine();
-
-         
-
-            //zrobic menu z pogladatem taskow
-            // w menu opcja utworzenia nowego taska
-            // taski maja zapisywac siew notatniku
 
         }
     }
