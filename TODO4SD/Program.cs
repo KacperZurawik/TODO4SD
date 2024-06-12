b88e152ea7f4b181e65083de57b3bd872e0c47ea
KacperZurawik <kaacper181@gmail.com>
2024-06-12 14:25:34 +0200

    New menu added

--- TODO4SD/Program.cs
+++ TODO4SD/Program.cs
@@ -10,7 +10,32 @@ namespace TODO4SD
     {
         static void Main(string[] args)
         {
+            MainMenu();
+        }
+        private static bool MainMenu()
+        {
+            Console.WriteLine("What you want to do?");
+            Console.WriteLine("1. Write down a task");
+            Console.WriteLine("2. Mark task as done");
+            Console.WriteLine("3. Show all tasks");
+            Console.WriteLine("4. Delete task");
+            Console.Write("\r\nPut the number (1-4): ");
 
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine();
+                    return true;
+                case "2":
+                    Console.WriteLine();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
+           
+            Console.ReadLine();
         }
     }
 }
