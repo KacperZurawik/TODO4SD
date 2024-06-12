b442dd8ca5d6bae4553a9b5421ed5fa0f04a0149
KacperZurawik <kaacper181@gmail.com>
2024-06-12 20:40:54 +0200

    Created main switch case menu

---
+++ TODO4SD/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO4SD
+{
+    internal class MainMenu
+    {
+
+        private static bool DisplayMenu()
+        {
+            Console.WriteLine("What you want to do?");
+            Console.WriteLine("1. Write down a task");
+            Console.WriteLine("2. Mark task as done");
+            Console.WriteLine("3. Show all tasks");
+            Console.WriteLine("4. Delete task");
+            Console.Write("\r\nPut the number (1-4): ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine();
+                    return true;
+                case "2":
+                    Console.WriteLine();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ TODO4SD/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO4SD
+{
+    internal class Menu
+    {
+
+        private void DisplayMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("TODO4SD");
+                Console.WriteLine("Main menu");
+                Console.WriteLine();
+
+                Console.WriteLine("1. Write down a new task");
+                Console.WriteLine("2. Show all tasks");
+                Console.WriteLine("3. Delete a task");
+                Console.WriteLine("4. Exit");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            Console.Clear();
+                            Console.WriteLine();
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            Console.WriteLine();
+                            break;
+                        }
+
+
+
+                }
+            }
+
+        }
+    }
+}
